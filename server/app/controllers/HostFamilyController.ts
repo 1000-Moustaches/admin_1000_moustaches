63dbd5110f9dbfbfc8d19572a1322f300e691f1f
Cédric Derache <c.derache@beapp.fr>
2025-06-09 16:34:35 +0200

    fix: hostFamily controller to populate hostFamilyKinds

--- server/app/controllers/HostFamilyController.ts
+++ server/app/controllers/HostFamilyController.ts
@@ -2,44 +2,44 @@ import { AppDataSource } from "../config/database"
 import { HostFamily } from "../models/HostFamily"
 
 export class HostFamilyController {
-    private hostFamilyRepository = AppDataSource.getRepository(HostFamily)
+  private hostFamilyRepository = AppDataSource.getRepository(HostFamily)
 
-    async getAllHostFamilies() {
-        return await this.hostFamilyRepository.find({
-            relations: {
-                hostFamilyKinds: false,
-                animalRelations: false,
-                referent: true
-            }
-        })
-    }
+  async getAllHostFamilies() {
+    return await this.hostFamilyRepository.find({
+      relations: {
+        hostFamilyKinds: true,
+        animalRelations: false,
+        referent: true
+      }
+    })
+  }
 
-    async getHostFamilyById(id: number) {
-        return await this.hostFamilyRepository.findOne({
-            where: { id },
-            relations: {
-                hostFamilyKinds: true,
-                animalRelations: {
-                    animal: {
-                        species: true
-                    }
-                },
-                referent: true
-            }
-        })
-    }
+  async getHostFamilyById(id: number) {
+    return await this.hostFamilyRepository.findOne({
+      where: { id },
+      relations: {
+        hostFamilyKinds: true,
+        animalRelations: {
+          animal: {
+            species: true
+          }
+        },
+        referent: true
+      }
+    })
+  }
 
-    async createHostFamily(hostFamilyData: Partial<HostFamily>) {
-        const hostFamily = this.hostFamilyRepository.create(hostFamilyData)
-        return await this.hostFamilyRepository.save(hostFamily)
-    }
+  async createHostFamily(hostFamilyData: Partial<HostFamily>) {
+    const hostFamily = this.hostFamilyRepository.create(hostFamilyData)
+    return await this.hostFamilyRepository.save(hostFamily)
+  }
 
-    async updateHostFamily(id: number, hostFamilyData: Partial<HostFamily>) {
-        await this.hostFamilyRepository.update(id, hostFamilyData)
-        return await this.getHostFamilyById(id)
-    }
+  async updateHostFamily(id: number, hostFamilyData: Partial<HostFamily>) {
+    await this.hostFamilyRepository.update(id, hostFamilyData)
+    return await this.getHostFamilyById(id)
+  }
 
-    async deleteHostFamily(id: number) {
-        return await this.hostFamilyRepository.delete(id)
-    }
+  async deleteHostFamily(id: number) {
+    return await this.hostFamilyRepository.delete(id)
+  }
 } 
