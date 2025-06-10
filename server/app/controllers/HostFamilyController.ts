dd5a05ddfa02e6f41620fb8814d73ee916df46c5
Gaetane Seguin <seguin.gaetane@gmail.com>
2025-06-10 19:16:05 +0200

    feat: create count host families available route

--- server/app/controllers/HostFamilyController.ts
+++ server/app/controllers/HostFamilyController.ts
@@ -1,7 +1,6 @@
 import { In } from "typeorm";
 import { AppDataSource } from "../config/database";
 import { HostFamily } from "../models/HostFamily";
-
 export class HostFamilyController {
   private hostFamilyRepository = AppDataSource.getRepository(HostFamily);
 
@@ -36,6 +35,32 @@ export class HostFamilyController {
         referent: true,
       },
     });
+  }
+
+  async getCountHostFamilyAvailable() {
+    const results = await this.hostFamilyRepository
+      .createQueryBuilder("hf")
+      .innerJoin("hf.hostFamilyKinds", "hfk")
+      .innerJoin("hfk.species", "s") // Join avec Species pour récupérer le nom
+      .select("COUNT(DISTINCT hf.id)", "count")
+      .addSelect("s.id", "id")
+      .where("hf.isAvailable = :isAvailable", { isAvailable: true })
+      .andWhere("hf.onBreak = :onBreak", { onBreak: false })
+      .groupBy("s.id")
+      .getRawMany();
+
+    const species = results.map((result) => ({
+      id: result.id,
+      name: result.name,
+      count: parseInt(result.count) || 0,
+    }));
+
+    const total = species.reduce((acc, s) => acc + s.count, 0);
+
+    return {
+      total,
+      species,
+    };
   }
 
   async createHostFamily(hostFamilyData: Partial<HostFamily>) {
